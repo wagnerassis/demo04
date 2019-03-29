866e64c05b8d3974dbdb9dfc110ccb23b752c5b9
Wagner Assis <wagner.carvalho@ilegra.com>
2019-03-29 18:37:34 -0300

    first

---
+++ Demo4all/Place/Views/PlaceDescCell.swift
@@ -0,0 +1,26 @@
+import Foundation
+import UIKit
+import TableManager
+import MapKit
+
+class PlaceDescCell: UITableViewCell {
+
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var serviceButton: UIButton!
+    @IBOutlet weak var addressButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationLogoView: UIImageView!
+    
+    static func newRow(placeID: String) -> Row {
+        let row = Row(identifier: "PlaceDescCell")
+        row.setHeight(withStaticHeight: 80)
+        row.setConfiguration { (row, cell, indexPath) in
+            guard let cell = cell as? PlaceDescCell else { return }
+            cell.addShadow()
+        }
+        return row
+    }
+}
